55a1fcdbf4686ceb31061c7558bb28bd2aa2bb6e
DavidDionysius <99daviddionysius@gmail.com>
2025-05-24 16:22:25 +0200

    Paramétrage de connexion à l'administrateur par point de vente

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ session_start();
 
         /* STYLYSATION DE FORMULAIRE */
         form{
-            background-color: rgba(0, 0, 150, 0.5);
+            background-color: rgba(0, 0, 150, 0.3);
             width: 40%;
             height: 350px;
             margin: 100px auto;
@@ -61,6 +61,10 @@ session_start();
             color: white;
         }
 
+        #erreur{
+            color: rgba(255, 0, 0, 1);
+        }
+
         /* ANNIMATION */
         /* #login{
             animation: tourner 3s 4;
@@ -77,6 +81,7 @@ session_start();
             }
         } */
     </style>
+    <script src="./js/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <form method="post">
@@ -94,15 +99,20 @@ session_start();
         <div>
             <button name="valider" id="bouton">Se connecter</button>
         </div>
+        <div>
+            <h4 id="erreur"></h4>
+        </div>
     </form>
 
     <!-- CONNEXION -->
      <?php
+     include("./bdd/connexion.php");
      if(isset($_POST["valider"])){
         $prenom = $_POST["prenom"];
         $mot_de_passe = $_POST["password"];
 
         if($prenom != "" && $mot_de_passe != ""){
+            // ADMINISTRATEUR PRINCIPAL
             if($prenom == "irestobar"){
                 if($mot_de_passe == "irestobar"){
                     $_SESSION["prenom"] = $_POST["prenom"];
@@ -111,10 +121,47 @@ session_start();
                     echo "Mot de passe incorrect";
                 }
             }else{
-                echo "Vérifier votre prénom";
+                // ADMINISTRATEUR PAR POINT DE VENTE
+                // SELECTION DE prenom ET mot_de_passe DANS LA TABLE admin_par_point_vente
+                $slct_par_point_vente = $connexion -> prepare("SELECT prenom, mot_de_passe FROM admin_par_point_vente WHERE prenom = :prenom");
+                $slct_par_point_vente -> execute([
+                    "prenom" => $prenom
+                ]);
+                $tab_point_vente = $slct_par_point_vente -> fetchAll();
+                $nb_adm_point_vente = count($tab_point_vente);
+                if($nb_adm_point_vente > 0){
+                    // SELECTION DU MOT DE PASSE
+                    $mot_passe_par_point_vente = $tab_point_vente[0]["mot_de_passe"];
+                    if($mot_passe_par_point_vente == $mot_de_passe){
+                        $_SESSION["prenom"] = $_POST["prenom"];
+                        header("location:admin_par_point_vente/gestion_employes/employe.php");
+                    }else{
+                        ?>
+                        <script>
+                            $(document).ready(function(){
+                                $("#erreur").text("Mot de passe incorrect!!!");
+                            })
+                        </script>
+                        <?php
+                    }
+                }else{
+                    ?>
+                    <script>
+                        $(document).ready(function(){
+                            $("#erreur").text("Ce compte n'existe pas!!!!");
+                        })
+                    </script>
+                    <?php
+                }
             }
         }else{
-            echo "Veuillez tout remplir";
+            ?>
+            <script>
+                $(document).ready(function(){
+                    $("#erreur").text("Veuillez tout remplir!!");
+                })
+            </script>
+            <?php
         }
      }
      ?>
